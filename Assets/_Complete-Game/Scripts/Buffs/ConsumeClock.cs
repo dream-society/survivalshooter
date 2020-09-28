2d375d0b25808cca81147ea5aa510cb8b1a3a95f
Michele Rossi <mikymod@gmail.com>
2020-09-28 19:05:57 +0200

    add clock

---
+++ Assets/_Complete-Game/Scripts/Buffs/ConsumeClock.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+
+    public class ConsumeClock : MonoBehaviour
+    {
+        GameObject player;
+        EnemyManager[] enemyManagers;
+
+        bool playerInRange = false;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            GameObject enemyManager = GameObject.FindGameObjectWithTag("EnemyManager");
+            enemyManagers = enemyManager.GetComponents<EnemyManager>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject == player)
+            {
+                playerInRange = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject == player)
+            {
+                playerInRange = false;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (playerInRange)
+            {
+                foreach (EnemyManager enemyManager in enemyManagers)
+                {
+                    enemyManager.SlowRateSpawn();
+                }
+
+                Destroy(gameObject);
+            }
+        }
+
+
+    }
+}
--- Assets/_Complete-Game/Scripts/Managers/EnemyManager.cs
+++ Assets/_Complete-Game/Scripts/Managers/EnemyManager.cs
@@ -9,13 +9,46 @@ namespace CompleteProject
         public float spawnTime = 3f;            // How long between each spawn.
         public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
 
+        bool slowed = false;
+        public float slowMultiplier = 2f;
+
+        float timer = 0.0f;
+
+        public float slowDuration;
+        float slowTimer = 0.0f;
 
         void Start ()
         {
-            // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-            InvokeRepeating ("Spawn", spawnTime, spawnTime);
+            slowDuration = spawnTime * slowMultiplier * 3;
         }
 
+        void Update()
+        {
+            timer += Time.deltaTime;
+            
+            if (slowed)
+            {
+                slowTimer += Time.deltaTime;
+                if (slowTimer > slowDuration)
+                {
+                    NormalRateSpawn();
+                }
+
+                if (timer >= spawnTime * slowMultiplier)
+                {
+                    Spawn();
+                    timer = 0.0f;
+                }
+            }
+            else
+            {
+                if (timer >= spawnTime)
+                {
+                    Spawn();
+                    timer = 0.0f;
+                }
+            }
+        }
 
         void Spawn ()
         {
@@ -31,6 +64,40 @@ namespace CompleteProject
 
             // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
             Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+            if (slowed)
+            {
+                SetEnemiesSpeed(0.6f);
+            }
+            else
+            {
+                SetEnemiesSpeed(2.0f);
+            }
+        }
+
+        public void SlowRateSpawn()
+        {
+            slowed = true;
+            slowTimer = 0.0f;
+
+            SetEnemiesSpeed(0.6f);
+        }
+
+        public void NormalRateSpawn()
+        {
+            slowed = false;
+            slowTimer = 0.0f;
+
+            SetEnemiesSpeed(2f);
+        }
+
+        private void SetEnemiesSpeed(float speed)
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies)
+            {
+                enemy.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = speed;
+            }
         }
     }
 }
